166a6229173821b97c8b5d3dec76e285763620c0
Darwin Smith <36823492+darwindeez@users.noreply.github.com>
2025-06-28 16:45:12 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('wpm-cache-v1').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon.png' // optional
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
